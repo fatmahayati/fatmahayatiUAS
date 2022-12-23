7ac72ba1ea3398385d45b3be9e93fc4cd6c5a950
fatmahayati <56388792+fatmahayati@users.noreply.github.com>
2022-12-23 11:03:46 +0700

    Update AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="about">
+        <h1>Biodata</h1>
+    <p> Nama : Fatma Hayati</p>
+    <p> Nim : 11950125061</p>
+  </div>
     <h1>This is an about page</h1>
   </div>
 </template>
