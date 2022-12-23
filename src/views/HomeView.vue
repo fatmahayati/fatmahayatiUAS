9721133c10838166a78977f501fab0738a6a5d8c
fatmahayati <56388792+fatmahayati@users.noreply.github.com>
2022-12-23 11:04:34 +0700

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,20 @@
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
+  <table class="table">
+<thead>
+  <tr>
+    <th>Nama</th>
+    <th>Nim</th>
+  </tr>
+</thead>
+<tbody>
+  <tr v-for="item in dataMahasiswa">
+    <td>{{item.nama}}</td>
+    <td>{{item.nim}}</td>
+  </tr>
+</tbody>
+</table>
 </template>
 
 <script>
